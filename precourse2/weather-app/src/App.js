2d4368fa4159ca6981e253943118b5ff37cfc60b
Joy <greeniejoy@gmail.com>
2019-10-09 10:04:35 -0500

    temporarily moved all click handles to seperate file and made getPosition funciton for geolocation

--- precourse2/weather-app/src/App.js
+++ precourse2/weather-app/src/App.js
@@ -20,69 +20,22 @@ class App extends React.Component {
       description: 'cloudy',
       temp: 75
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(location) {
-    this.setState({ location: location.target.value });
-    // console.log(location.target.value);
+  getPosition = () => {
+    return new Promise(function (resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
   }
-  // function App() {
-  handleSubmit(event) {
-    //This prevents page from re-loading when clicked
-    event.preventDefault();
-    // console.log(event);
-    const { target: { value } } = event;
-
-    // And do whatever you need with it's value, for example change state 
-    this.setState({ location: value });
-    // const data = new FormData();
-    // data.append('location', location.target);
-    console.log('clicked successfully');
-    // console.log(data);
-    // Access FormData fields with `data.get(fieldName)`
-  }
-
-  // function handleClick(e) {
-  //   this.setState({ input: e.target.value })
-  // axios.post('/api/location')
-  //   .then(response => console.log('response words'))
-  //   .catch(err => console.error(err));
-
-  // fetch('/api/location', {
-  //   type: 'POST',
-  //   body: data
-  // })
-  //   console.log("clicked successfully");
-  // }
 
   async componentDidMount() {
     let API = "https://cors-anywhere.herokuapp.com/fcc-weather-api.glitch.me/api/current?lon=118.2437&lat=34.0522";
     const response = await fetch(API);
     const data = await response.json();
-    this.setState({ description: data.weather[0].description });
+    this.setState({ description: data.weather[0].description, temp: data.main.temp });
     console.log(data);
-    // let weather = API.get('/', {
-    //   main description: "",
-    //   temp: 0,
-    //   img: ''
   };
 
-
-
-  //     // Parse the results for ease of use.
-  // userData = userData.data.results[0];
-
-  //   this.setState({
-  //     ...this.state, ...{
-  //       isLoading: false,
-  //       name,
-  //       avatar,
-  //       email
-  //     }
-  //   });
-  // }
 
   render() {
     return (
@@ -96,23 +49,7 @@ class App extends React.Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-
           </a>
-
-
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Location:
-            <input type="text" location={this.state.location} onChange={this.handleChange} />
-            </label>
-            {/* //When I do it this way it rerenders after every click */}
-            {/* <input type="submit" value="Submit" onClick={handleSubmit} /> */}
-          </form>
-          {/* When I do the button on click it does not re-render */}
-          <button onClick={this.handleSubmit}>
-            Click me!
-    </button>
-          <div id="weather">{this.state.description}</div>
         </body>
       </div>
     );
